74099e736661ca5ca2efd60cd6787cc54a719433
vara <peddaramvaralakshmi01@gmail.com>
2024-07-30 16:26:53 +0530

    chagedloginfile

--- src/main/java/com/qa/opencart/pages/LoginPage.java
+++ src/main/java/com/qa/opencart/pages/LoginPage.java
@@ -6,61 +6,60 @@ import org.openqa.selenium.WebDriver;
 import com.qa.opencart.constants.AppConstants;
 import com.qa.opencart.utils.ElementUtil;
 import com.qa.opencart.utils.TimeUtil;
+import io.qameta.allure.Step;
 
 public class LoginPage {
 
 	private WebDriver driver;
 	private ElementUtil eleUtil;
-	//1.Page Objects : By Locators
-	
-	private By emailId=By.id("input-email");
+
+	// 1. Page Objects: By Locators:
+
+	private By emailId = By.id("input-email");
 	private By password = By.id("input-password");
-	private By loginBtn=By.xpath("//input[@value='Login']");
-	private By forgotPwdLink=By.linkText("Forgotten Password");
-	private By registerLink=By.linkText("Register");
-	
-	//2. public constructor of the page
-	public LoginPage(WebDriver driver)
-	{
-		this.driver=driver;
-		eleUtil =new ElementUtil(driver);
+	private By loginBtn = By.xpath("//input[@value='Login']");
+	private By forgotPwdLink = By.linkText("Forgotten Password");
+	private By registerLink = By.linkText("Register");
+
+	// 2. public const.. of the page:
+	public LoginPage(WebDriver driver) {
+		this.driver = driver;
+		eleUtil = new ElementUtil(driver);
 	}
-	
-	//3. public page Actions/Methods{features of this paticular page}
-	public 	String getLoginPageTitle()
-	{
-		String title=eleUtil.waitForTitleToBe(AppConstants.LOGIN_PAGE_TITLE, TimeUtil.DEFAULT_TIME);
-		System.out.println("login page title:"+title);
+
+	// 3. public page actions/methods
+	@Step("getting login page title value...")
+	public String getLoginPageTitle() {
+		String title = eleUtil.waitForTitleToBe(AppConstants.LOGIN_PAGE_TITLE, TimeUtil.DEFAULT_TIME);
+		System.out.println("login page title : " + title);
 		return title;
 	}
-	public 	String getLoginPageURL()
-	{
-		String url=eleUtil.waitForURLContains(AppConstants.ACC_PAGE_FRACTION_URL,TimeUtil.DEFAULT_TIME);
-		System.out.println("login page title:"+url);
+
+	@Step("getting login page URL...")
+	public String getLoginPageURL() {
+		String url = eleUtil.waitForURLContains(AppConstants.LOGIN_PAGE_FRACTION_URL, TimeUtil.DEFAULT_TIME);
+		System.out.println("login page url : " + url);
 		return url;
 	}
-	
-	public boolean checkForgotPwdLinkExists()
-	{
+
+	@Step("getting the state of forgot pwd link exist...")
+	public boolean checkForgotPwdLinkExist() {
 		return eleUtil.doIsDisplayed(forgotPwdLink);
-		
 	}
-	public AccountsPage doLogin(String username,String pwd)
-	{
-		//System.out.println("user creds:"+username+":"+pwd);
-		eleUtil.doSendKeys(emailId,username,TimeUtil.DEFAULT_MEDIUM_TIME);
-		eleUtil.doSendkeys(password, pwd);
+
+	@Step("login to application with username: {0} and password: {1}")
+	public AccountsPage doLogin(String username, String pwd) {
+		System.out.println("user creds : " + username + ":" + pwd);
+		eleUtil.doSendKeys(emailId, username, TimeUtil.DEFAULT_MEDIUM_TIME);
+		eleUtil.doSendKeys(password, pwd);
 		eleUtil.doClick(loginBtn);
 		return new AccountsPage(driver);
-//		String title= driver.getTitle();
-//		System.out.println("Account page title:"+title);
-//		return title;
+
 	}
-	public RegisterPage navigateToRegisterPage()
-	{
-		eleUtil.doClick(registerLink,TimeUtil.DEFAULT_TIME);
+
+	@Step("navigating to register page...")
+	public RegisterPage navigateToRegisterPage() {
+		eleUtil.doClick(registerLink, TimeUtil.DEFAULT_TIME);
 		return new RegisterPage(driver);
 	}
-	
-	
 }
--- src/test/java/com/qa/opencart/tests/LoginPageTest.java
+++ src/test/java/com/qa/opencart/tests/LoginPageTest.java
@@ -6,30 +6,58 @@ import org.testng.annotations.Test;
 import com.qa.opencart.constants.AppConstants;
 import com.qa.opencart.base.BaseTest;
 import com.qa.opencart.errors.AppError;
+import io.qameta.allure.Description;
+import io.qameta.allure.Epic;
+import io.qameta.allure.Feature;
+import io.qameta.allure.Issue;
+import io.qameta.allure.Owner;
+import io.qameta.allure.Severity;
+import io.qameta.allure.SeverityLevel;
+import io.qameta.allure.Story;
 
 public class LoginPageTest extends BaseTest {
 
+	@Description("checking login page title test ----")
+	@Severity(SeverityLevel.MINOR)
+	@Owner("Naveen Automation Labs")
+	@Issue("Login-1234")
 	@Test(priority = 1)
 	public void loginPageTitleTest() {
 		String actTitle = loginPage.getLoginPageTitle();
 		Assert.assertEquals(actTitle, AppConstants.LOGIN_PAGE_TITLE, AppError.TITLE_NOT_FOUND);
 	}
 
+	@Description("checking login page url ----")
+	@Severity(SeverityLevel.NORMAL)
+	@Owner("Naveen Automation Labs")
 	@Test(priority = 2)
 	public void loginPageURLTest() {
 		String actURL = loginPage.getLoginPageURL();
 		Assert.assertTrue(actURL.contains(AppConstants.LOGIN_PAGE_FRACTION_URL), AppError.URL_NOT_FOUND);
 	}
 
+	@Description("checking forgot password link exist on the login page ----")
+	@Severity(SeverityLevel.CRITICAL)
+	@Owner("Naveen Automation Labs")
 	@Test(priority = 3)
 	public void forgotPwdLinkExistTest() {
-		Assert.assertTrue(loginPage.checkForgotPwdLinkExists(), AppError.ELEMENT_NOT_FOUND);
+		Assert.assertTrue(loginPage.checkForgotPwdLinkExist(), AppError.ELEMENT_NOT_FOUND);
 	}
 
+	@Description("checking user is able to login to app successfully ----")
+	@Severity(SeverityLevel.BLOCKER)
+	@Owner("Naveen Automation Labs")
 	@Test(priority = 4)
 	public void loginTest() {
-	accPage= loginPage.doLogin(prop.getProperty("username"), prop.getProperty("password"));
-	Assert.assertEquals(accPage.getAccPageTitle(),AppConstants.ACCOUNTS_PAGE_TITLE,AppError.TITLE_NOT_FOUND);
-		
+		accPage = loginPage.doLogin(prop.getProperty("username"), prop.getProperty("password"));
+		Assert.assertEquals(accPage.getAccPageTitle(), AppConstants.ACCOUNTS_PAGE_TITLE, AppError.TITLE_NOT_FOUND);
 	}
+	
+	@Test
+	public void mytest() {
+		Assert.assertEquals(true, false);
+	}
+	
+	
+
 }
